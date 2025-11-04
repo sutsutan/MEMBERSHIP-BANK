ae398758ae430b5438e24eafc5bbd84843c5a0c6
sutsutan <sutsutan9@gmail.com>
2025-11-04 09:58:57 +0700

    dsauid

--- frontend-mbank/src/pages/AdminPanel.jsx
+++ frontend-mbank/src/pages/AdminPanel.jsx
@@ -1,6 +1,6 @@
 export default function AdminPanel() {
     return (
-      <div className="bg-white p-6 rounded-2xl shadow-md">
+      <div className="bg-white p-6 rounded-2xl shadow-md text-black">
         <h1 className="text-2xl font-semibold mb-4">Panel Admin / Teller</h1>
         <p>Selamat datang di dashboard admin.</p>
       </div>
